32d50370e6bdfa3eb3a84b3489687a45ffc82777
Enes Çetintaş <enesctnts@gmail.com>
2022-03-24 14:12:19 +0300

    MVC giriş yapildi

--- MvcUI/Controllers/CategoryController.cs
+++ MvcUI/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,13 @@ namespace MvcUI.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        CategoryManager categoryManager = new CategoryManager();
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = categoryManager.GetAll();
+            return View(categoryvalues);
         }
     }
 }
