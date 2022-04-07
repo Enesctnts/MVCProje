f01018b34917eccd4be0f80e5dc7fb9982105534
Enes Çetintaş <enesctnts@gmail.com>
2022-04-08 00:05:01 +0300

    HeadingController Created and AdminLayout updated

---
+++ MvcUI/Controllers/HeadingController.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class HeadingController : Controller
+    {
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
+        // GET: Heading
+        public ActionResult Index()
+        {
+            var headingvalues = headingManager.GetList();
+            return View(headingvalues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in categoryManager.GetList()
+                                                  select new SelectListItem
+                                                  {
+                Text=x.CategoryName,
+                Value=x.CategoryId.ToString()
+                                                  }).ToList();
+            List<SelectListItem> valuewriter = (from x in writerManager.GetList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName + " " + x.WriterSurname,
+                                                    Value = x.WriterId.ToString()
+                                                }).ToList();
+            
+            ViewBag.vlc = valuecategory;
+            ViewBag.vlw = valuewriter;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortTimeString());
+            headingManager.HeadingAddBl(heading);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
