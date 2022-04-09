c665f3b06c1ef4bdaf738ba1e528c6554d9e4a0d
Enes Çetintaş <enesctnts@gmail.com>
2022-04-09 19:00:27 +0300

    HeadingStatus added and Heading Updated

--- EntityLayer/Concrete/Heading.cs
+++ EntityLayer/Concrete/Heading.cs
@@ -19,6 +19,8 @@ namespace EntityLayer.Concrete
         public string HeadingName { get; set; }
         public DateTime HeadingDate { get; set; }
 
+        public bool HeadingStatus { get; set; }
+
         public virtual Category Category { get; set; }
         public virtual Writer Writer { get; set; }
 
