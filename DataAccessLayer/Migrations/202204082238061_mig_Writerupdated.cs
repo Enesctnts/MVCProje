699ce684fdc3ac26ce808d7483570d3925e3d331
Enes Çetintaş <enesctnts@gmail.com>
2022-04-09 03:04:21 +0300

    Writer Updated with migration

---
+++ DataAccessLayer/Migrations/202204082238061_mig_Writerupdated.cs
@@ -0,0 +1,20 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig_Writerupdated : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Writers", "WriterAbout", c => c.String(maxLength: 250));
+            AlterColumn("dbo.Writers", "Password", c => c.String(maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Writers", "Password", c => c.String(maxLength: 200));
+            AlterColumn("dbo.Writers", "WriterAbout", c => c.String(maxLength: 100));
+        }
+    }
+}
