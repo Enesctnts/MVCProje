508926e0e58a59972bc41039ae8073e3c914e90c
Enes Çetintaş <enesctnts@gmail.com>
2022-04-09 19:02:06 +0300

    ContentController Create

---
+++ MvcUI/Controllers/ContentController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class ContentController : Controller
+    {
+        // GET: Content
+        ContentManager contentManager = new ContentManager( new EfContentDal());
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ContentByHeading(int id)
+        {
+            var contentvalues = contentManager.GetListHeadingId(id);
+            return View(contentvalues);
+        }
+    }
+}
