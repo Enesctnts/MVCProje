d11f6c36559ace25f5d4848dd891a46ee6a9f721
Enes Çetintaş <enesctnts@gmail.com>
2022-04-10 19:32:43 +0300

    About page created and AboutPartials page added.

---
+++ MvcUI/Controllers/AboutController.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+        public ActionResult Index()
+        {
+            var aboutvalues = aboutManager.GetList();
+            return View(aboutvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAbout(About about)
+        {
+            aboutManager.AboutAddBl(about);
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult AboutPartial()
+        {
+            return PartialView();
+        }
+
+    }
+}
