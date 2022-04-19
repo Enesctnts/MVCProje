c091d20cab300418d6f4d5a6e651d67aa574f411
Enes Çetintaş <enesctnts@gmail.com>
2022-04-20 00:00:26 +0300

    Mesaj sayfası için MessageController oluuşturuldu ve Actionlar yapıldı.

---
+++ MvcUI/Controllers/MessageController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class MessageController : Controller
+    {
+        MessageManager cm = new MessageManager(new EfMessageDal());
+        public ActionResult Inbox()
+        {
+            var messagelist = cm.GetListInbox();
+            return View(messagelist);
+        }
+
+        public ActionResult Sendbox()
+        {
+            TempData[]
+            var messagelist = cm.GetListSendbox();
+            return View(messagelist);
+        }
+
+
+    }   
+}
