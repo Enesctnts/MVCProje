1685973b04fbbd8faf68d81a2f070fa991952768
Enes Çetintaş <enesctnts@gmail.com>
2022-04-19 23:53:58 +0300

    Mesaj sayfası oluşturuldu.

---
+++ BusinessLayer/Abstract/IMessageService.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IMessageService
+    {
+        List<Message> GetListInbox();
+        List<Message> GetListSendbox();
+        void MessageAddBl(Message message);
+        void MessageDelete(Message message);
+        Category GetById(int id);
+        void MessageUpdate(Message message);
+
+
+    }
+}
