9f4274a5fd46c5f1746367855ddf3583a991a83b
Enes Çetintaş <enesctnts@gmail.com>
2022-04-19 23:54:41 +0300

    Mesaj sayfası için CRUD yapıldı.

---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Message> GetListInbox()
+        {
+            return _messageDal.List(x => x.ReceiverMail == "admin@gmail.com");
+        }
+
+
+        public List<Message> GetListSendbox()
+        {
+            return _messageDal.List(x => x.SenderMail == "admin@gmail.com");
+        }
+
+        public void MessageAddBl(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageDelete(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageUpdate(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
