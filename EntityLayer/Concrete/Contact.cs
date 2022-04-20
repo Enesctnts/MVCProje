86c9e65a8d58d02a438e199195f21bcbd1ff47d6
Enes Çetintaş <enesctnts@gmail.com>
2022-04-20 03:56:06 +0300

    Contact tablosuna tarih kolonu eklendi.

--- EntityLayer/Concrete/Contact.cs
+++ EntityLayer/Concrete/Contact.cs
@@ -22,6 +22,8 @@ namespace EntityLayer.Concrete
         [StringLength(50)]
         public string Subject { get; set; }
 
+        public DateTime ContactDate { get; set; }
+
         [StringLength(200)]
         public string Message { get; set; }
 
