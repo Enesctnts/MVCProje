9fce3a084ee3642f401822329a69b96dc2e86a00
Enes Çetintaş <enesctnts@gmail.com>
2022-04-20 04:37:01 +0300

    GalleryController oluşturuldu. Yazar fotoğraflarının listelenmesi için IndexAction yazıldı.

---
+++ MvcUI/Controllers/GalleryController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class GalleryController : Controller
+    {
+        ImageFileManager ımageFileManager = new ImageFileManager(new EfImageFileDal());
+
+        public ActionResult Index()
+        {
+            var files = ımageFileManager.GetList();
+            return View(files); 
+        }
+    }
+}
