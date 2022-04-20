e067a1f9d01b7df9125511ba939ab8a5b1e17a9e
Enes Çetintaş <enesctnts@gmail.com>
2022-04-20 03:50:05 +0300

    Yazar fotoğraflarını listelemek için  ImageFileManager yazılarak projeye dahil edildi.

---
+++ BusinessLayer/Concrete/ImageFileManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ImageFileManager : IImageFileService
+    {
+        IImageFileDal _ımageFileDal;
+
+        public ImageFileManager(IImageFileDal ımageFileDal)
+        {
+            _ımageFileDal = ımageFileDal;
+        }
+
+        public List<ImageFile> GetList()
+        {
+            return _ımageFileDal.List();
+        }
+    }
+}
