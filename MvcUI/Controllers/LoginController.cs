8fc67767ae99b3d02d44656116d34458ffd86562
Enes Çetintaş <enesctnts@gmail.com>
2022-04-22 01:34:23 +0300

    LoginController oluşturuldu. Authentication yapıldı.

---
+++ MvcUI/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin admin)
+        {
+            Context context = new Context();
+            var adminUserInfo = context.Admins.FirstOrDefault(x => x.AdminUserName == admin.AdminUserName && x.AdminPassword == admin.AdminPassword);
+            if (adminUserInfo!=null)
+            {
+                FormsAuthentication.SetAuthCookie(adminUserInfo.AdminUserName, false);
+                Session["AdminUserName"] = adminUserInfo.AdminUserName;
+                return RedirectToAction("Index", "AdminCategory");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+       
+
+    }
+}
