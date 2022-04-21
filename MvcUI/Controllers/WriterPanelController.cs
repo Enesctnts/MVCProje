79dcbd475436f28564a84b7d7c17bc85cd593879
Enes Çetintaş <enesctnts@gmail.com>
2022-04-22 01:36:48 +0300

    Yazar için yaxar paneli eklendi. Devam ediyorum..

---
+++ MvcUI/Controllers/WriterPanelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class WriterPanelController : Controller
+    {
+        // GET: WriterPanel
+        public ActionResult WriterProfile()
+        {
+            return View();
+        }
+    }
+}
