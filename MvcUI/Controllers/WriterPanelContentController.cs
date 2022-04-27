ee8d65e296905ce3ed19a60c4e07dd81d1cb0d4c
Enes Çetintaş <enesctnts@gmail.com>
2022-04-27 22:37:06 +0300

    Yazar paneli oluşturuldu.

---
+++ MvcUI/Controllers/WriterPanelContentController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class WriterPanelContentController : Controller
+    {
+        ContentManager contentManager = new ContentManager(new EfContentDal());
+
+        public ActionResult MyContent(string p)
+        {
+            Context c = new Context();
+            p = (string)Session["WriterMail"];
+            var writeridinfo = c.Writers.Where(x => x.WriterMail == p).Select(y => y.WriterId).FirstOrDefault().Value;
+            var contentvalues = contentManager.GetListByWriter(writeridinfo);
+            return View(contentvalues);
+        }
+    }
+}
