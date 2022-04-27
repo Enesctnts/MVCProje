3eda3fed24d24627ec22b5bd51965a42883daf4e
Enes Çetintaş <enesctnts@gmail.com>
2022-04-27 22:38:20 +0300

    Yazar paneli için sayfa oluşturuldu.

--- MvcUI/Controllers/WriterPanelController.cs
+++ MvcUI/Controllers/WriterPanelController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,78 @@ namespace MvcUI.Controllers
 {
     public class WriterPanelController : Controller
     {
-        // GET: WriterPanel
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        Context c = new Context();
+
         public ActionResult WriterProfile()
         {
             return View();
         }
+
+        public ActionResult MyHeading(string p)
+        {
+            
+            p = (string)Session["WriterMail"];
+            var writeridinfo = c.Writers.Where(x => x.WriterMail == p).Select(y => y.WriterId).FirstOrDefault().Value;
+            var values = headingManager.GetListByWriter(writeridinfo);
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in categoryManager.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryId.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewHeading(Heading heading)
+        {
+            string writermailinfo = (string)Session["WriterMail"];
+            var writeridinfo = c.Writers.Where(x => x.WriterMail == writermailinfo).Select(y => y.WriterId).FirstOrDefault().Value;
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            heading.WriterId = writeridinfo;
+            heading.HeadingStatus = true;
+            headingManager.HeadingAddBl(heading);
+            return RedirectToAction("MyHeading");
+        }
+
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valuecategory = (from x in categoryManager.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryId.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            var HeadingValue = headingManager.GetById(id);
+            return View(HeadingValue);
+        }
+
+        [HttpPost]
+        public ActionResult EditHeading(Heading heading)
+        {
+
+            headingManager.HeadingUpdate(heading);
+            return RedirectToAction("MyHeading");
+        }
+
+        public ActionResult DeleteHeading(int id)
+        {
+            var headingvalue = headingManager.GetById(id);
+            headingvalue.HeadingStatus = false;
+            headingManager.HeadingUpdate(headingvalue);
+            return RedirectToAction("MyHeading");
+        }
+
     }
 }
