2500cf38ccb88d2f7cbd87626adee01e63e56a17
Enes Çetintaş <enesctnts@gmail.com>
2022-04-27 22:35:08 +0300

    Kullanıcıların kullanacağı vitrin oluşturulmaya başlandı.

---
+++ MvcUI/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        public ActionResult Headings()
+        {
+            var headinglist = headingManager.GetList();
+            return View(headinglist);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
