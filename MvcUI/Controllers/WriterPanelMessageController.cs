b5d9a6294f6577fa4445c4fd98b5eda57b2d57a5
Enes Çetintaş <enesctnts@gmail.com>
2022-04-27 22:39:08 +0300

    Yazar panelinin mesaj sayfası oluşturuldu.

---
+++ MvcUI/Controllers/WriterPanelMessageController.cs
@@ -0,0 +1,76 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class WriterPanelMessageController : Controller
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        MessageValidator messageValidator = new MessageValidator();
+        public ActionResult Inbox()
+        {
+            TempData["InboxCount"] = messageManager.GetListInbox().Count();
+            var messagelist = messageManager.GetListInbox();
+            return View(messagelist);
+        }
+
+        public ActionResult Sendbox()
+        {
+            TempData["SendCount"] = messageManager.GetListSendbox().Count();
+            var messagelist = messageManager.GetListSendbox();
+            return View(messagelist);
+        }
+
+        public PartialViewResult MessageListMenu()
+        {
+
+            return PartialView();      
+        }
+
+        public ActionResult GetInBoxMessageDetails(int id)
+        {
+            var values = messageManager.GetById(id);
+            return View(values);
+        }
+
+        public ActionResult GetSendBoxMessageDetails(int id)
+        {
+            var values = messageManager.GetById(id);
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message message)
+        {
+            ValidationResult results = messageValidator.Validate(message);
+            if (results.IsValid)
+            {
+                message.SenderMail = "enesctnts@gmail.com";
+                message.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                messageManager.MessageAdd(message);
+                return RedirectToAction("Sendbox");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
