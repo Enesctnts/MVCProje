2508d3edfb849eaa4fea0bbfa6448a0183abdb2a
Enes Çetintaş <enesctnts@gmail.com>
2022-04-29 02:13:20 +0300

    Yeni İçerik Girişi işlemi yapıldı.

--- MvcUI/Controllers/WriterPanelContentController.cs
+++ MvcUI/Controllers/WriterPanelContentController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +13,39 @@ namespace MvcUI.Controllers
     public class WriterPanelContentController : Controller
     {
         ContentManager contentManager = new ContentManager(new EfContentDal());
-
+        Context c = new Context();
         public ActionResult MyContent(string p)
         {
-            Context c = new Context();
+            
             p = (string)Session["WriterMail"];
             var writeridinfo = c.Writers.Where(x => x.WriterMail == p).Select(y => y.WriterId).FirstOrDefault().Value;
             var contentvalues = contentManager.GetListByWriter(writeridinfo);
             return View(contentvalues);
         }
+        [HttpGet]
+        public ActionResult AddContent(int id)
+        {
+            ViewBag.d = id;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddContent(Content content)
+        {
+            string mail = (string)Session["WriterMail"];
+            var writeridinfo = c.Writers.Where(x => x.WriterMail == mail).Select(y => y.WriterId).FirstOrDefault().Value;
+            var contentvalues = contentManager.GetListByWriter(writeridinfo);
+            content.ContentDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            content.WriterId = writeridinfo;
+            content.ContentStatus = true;
+            contentManager.ContentAddBl(content);
+            return RedirectToAction("MyContent");
+        }
+
+        public ActionResult ToDoList()
+        {
+            return View();
+        }
+
     }
 }
