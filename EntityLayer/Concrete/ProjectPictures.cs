5563aadbfd20f8978cfacff7282327a0a9067693
Enes Çetintaş <enesctnts@gmail.com>
2022-05-03 23:57:24 +0300

    Proje tanıtım sayfasında proje fotoğraflarının listelenmesi için ProjectPictures oluşturuldu.

---
+++ EntityLayer/Concrete/ProjectPictures.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    [Table("ProjectPictures")]
+    public class ProjectPictures : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(500, ErrorMessage = "Ürün resim yolu en fazla 400 karakter")]
+        public string Picture { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+
+    }
+}
