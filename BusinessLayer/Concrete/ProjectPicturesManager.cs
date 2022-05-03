40c50af894f324d00dc022e8a824fde7ccc18179
Enes Çetintaş <enesctnts@gmail.com>
2022-05-03 23:51:15 +0300

    Proje fotoğraflarının tanıtım sayfasında listelenmesi için Manager yazıldı.

---
+++ BusinessLayer/Concrete/ProjectPicturesManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ProjectPicturesManager : IProjectPicturesService
+    {
+        IProjectPicturesDal _projectPicturesDal;
+
+        public ProjectPicturesManager(IProjectPicturesDal projectPicturesDal)
+        {
+            _projectPicturesDal = projectPicturesDal;
+        }
+
+        public List<ProjectPictures> GetList()
+        {
+            return _projectPicturesDal.List();
+        }
+    }
+}
