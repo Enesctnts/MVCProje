b2d45035672a104992fce9120b87156f3fb78b07
Enes Çetintaş <enesctnts@gmail.com>
2022-05-04 16:46:13 +0300

    Kaydet butonu için Alert eklendi. Butona bastıktan sonra popup ekranı gibi ekranda tebrikler işlem kaydedildi tarzında mesaj veriyor.

---
+++ MvcUI/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SweetAlert()
+        {
+            return View();
+        }
+
+    }
+}
