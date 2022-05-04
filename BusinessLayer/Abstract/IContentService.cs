8750052c242067a34e097cf0fd1977504ba012eb
Enes Çetintaş <enesctnts@gmail.com>
2022-05-04 16:41:06 +0300

    Başlıklar için arama kutusu yapıldı.

--- BusinessLayer/Abstract/IContentService.cs
+++ BusinessLayer/Abstract/IContentService.cs
@@ -9,7 +9,7 @@ namespace BusinessLayer.Abstract
 {
     public interface IContentService
     {
-        List<Content> GetList();
+        List<Content> GetList(string p);
         List<Content> GetListByWriter(int id);
         List<Content> GetListHeadingId(int id);//Başlığa göre liste getirecek.
         void ContentAddBl(Content content);
