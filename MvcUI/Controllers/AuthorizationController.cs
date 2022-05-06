da573cb27fc4b59e1241fd21abd9a2a7e71a60a8
Enes Çetintaş <enesctnts@gmail.com>
2022-05-06 22:12:53 +0300

    Admin listeleme işlemi yapıldı.

---
+++ MvcUI/Controllers/AuthorizationController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+
+        AdminManager adminManager = new AdminManager(new EfAdminDal());
+        public ActionResult Index()
+        {
+            var adminvalues = adminManager.GetList();
+            return View(adminvalues);
+        }
+    }
+}
