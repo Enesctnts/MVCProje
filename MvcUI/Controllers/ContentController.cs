21fc69c61b140b29d1372bf5a73e8243a1ee67c9
Enes Çetintaş <enesctnts@gmail.com>
2022-05-06 22:20:30 +0300

    AramaKutusu işlemi için bütün içerikler getirildi.

--- MvcUI/Controllers/ContentController.cs
+++ MvcUI/Controllers/ContentController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,13 @@ namespace MvcUI.Controllers
         {
             return View();
         }
+         
+        public ActionResult GetAllContent(string p)
+        {
+            var values = contentManager.GetList(p);
+            return View(values);
+        }
+
         public ActionResult ContentByHeading(int id)
         {
             var contentvalues = contentManager.GetListHeadingId(id);
