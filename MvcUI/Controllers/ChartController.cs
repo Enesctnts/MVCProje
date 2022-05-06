8b14efd4e1b61fb6dfbd1bbf00e6f67d3a865c16
Enes Çetintaş <enesctnts@gmail.com>
2022-05-06 22:19:05 +0300

    Kategoriler için grafik oluşturma işlemleri yapıldı

---
+++ MvcUI/Controllers/ChartController.cs
@@ -0,0 +1,52 @@
+﻿using MvcUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<CategoryClass> BlogList()
+        {
+            List<CategoryClass> categoryClasses = new List<CategoryClass>();
+            categoryClasses.Add(new CategoryClass()
+            {
+                CategoryName = "Yazımlım",
+                CategoryCount = 8
+            });
+            categoryClasses.Add(new CategoryClass()
+            {
+                CategoryName = "Seyahat",
+                CategoryCount = 4
+            });
+            categoryClasses.Add(new CategoryClass()
+            {
+                CategoryName = "Teknoloji",
+                CategoryCount = 7
+            });
+            categoryClasses.Add(new CategoryClass()
+            {
+                CategoryName = "Spor",
+                CategoryCount = 1
+            });
+
+            return categoryClasses;
+
+        }
+
+    }
+}
