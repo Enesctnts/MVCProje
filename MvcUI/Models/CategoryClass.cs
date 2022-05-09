0b9e334beced6e1a49e24e7a841aad4968d440a1
Enes Çetintaş <enesctnts@gmail.com>
2022-05-09 23:50:17 +0300

    CategoryClass' ı kategoriler grafiği oluşturmak için oluşturuldu.

--- MvcUI/Models/CategoryClass.cs
+++ MvcUI/Models/CategoryClass.cs
@@ -7,6 +7,7 @@ namespace MvcUI.Models
 {
     public class CategoryClass
     {
+        //kategori grafiği oluşturmak için oluşturuldu.
         public string CategoryName  { get; set; }
         public int CategoryCount  { get; set; }
 
